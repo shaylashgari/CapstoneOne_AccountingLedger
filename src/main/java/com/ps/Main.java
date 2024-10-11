c9a286288e0eb3745b382d686354e5a52ed6cf1b
Shay <shaylashgari@gmail.com>
2024-10-11 15:52:01 -0500

    latest

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,59 @@
 package com.ps;
 
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    // Begin create menu Instructions
+
+    // initialize the command variable
+
+    int mainMenuCommand;
+
+    do {
+
+        // display menu
+        System.out.println("Please enter an option:");
+        System.out.println("1) Add Deposit ");
+        System.out.println("2) Make Payment (Debit)");
+        System.out.println("3) Ledger");
+        System.out.println("4) Exit";
+
+        // import scanner, create instance of scanner, use scanner to store user's command as variable
+
+        try {
+            Scanner commandScanner;
+            mainMenuCommand = commandScanner.nextInt();
+        } catch (InputMismatchException ime) {
+            mainMenuCommand = 0;
+        }
+
+        // switch statement to match the user command to the provided cases
+
+        switch (mainMenuCommand) {
+            case 1:
+                addDeposit();
+                break;
+            case 2:
+                makeDeposit();
+                break;
+            case 3:
+                ledger();
+                break;
+            case 4:
+                System.out.println("Exiting");
+                break;
+            default:
+                //handle incorrect commands
+                System.out.println("Command not found, please try again.");
+        }
+
+    } while (mainMenuCommand != 0);
+
     }
+    public static void addDeposit(){
+
 }
